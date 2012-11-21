9e32a7c8b4e0f68c12a7d1e998da2fc7547ae780
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-11-21 13:04:53 -0500

    Added first version of nginx-sg

---
+++ Formula/nginx-sg.rb
@@ -0,0 +1,155 @@
+require 'formula'
+
+class NginxSg < Formula
+  homepage 'http://nginx.org/'
+  url 'http://nginx.org/download/nginx-1.2.4.tar.gz'
+  sha1 'e3de0b2b82095f26e96bdb461ba36472d3e7cdda'
+
+  devel do
+    url 'http://nginx.org/download/nginx-1.3.8.tar.gz'
+    sha1 '84ff39e3f76e9f496f4e05080885e04caf472bb9'
+  end
+
+  env :userpaths
+
+  depends_on 'pcre'
+
+  option 'with-passenger', 'Compile with support for Phusion Passenger module'
+  option 'with-webdav', 'Compile with support for WebDAV module'
+
+  skip_clean 'logs'
+
+  # Changes default port to 8080
+  def patches
+    DATA
+  end
+
+  def collect_modules regex=nil
+    ARGV.select { |arg| arg.match(regex) != nil }.collect { |arg| arg.gsub(regex, '') }
+  end
+
+  def nginx_modules; collect_modules(/^--include-module-/); end
+  def add_from_github; collect_modules(/^--add-github-module=/); end
+  def add_from_mdounin; collect_modules(/^--add-mdounin-module=/); end
+
+  def passenger_config_args
+    passenger_root = `passenger-config --root`.chomp
+
+    if File.directory?(passenger_root)
+      return "--add-module=#{passenger_root}/ext/nginx"
+    end
+
+    puts "Unable to install nginx with passenger support. The passenger"
+    puts "gem must be installed and passenger-config must be in your path"
+    puts "in order to continue."
+    exit
+  end
+
+  def install
+    args = [
+      "--prefix=#{prefix}",
+      "--with-http_ssl_module",
+      "--with-pcre",
+      "--with-ipv6",
+      "--with-cc-opt=-I#{HOMEBREW_PREFIX}/include",
+      "--with-ld-opt=-L#{HOMEBREW_PREFIX}/lib",
+      "--conf-path=#{etc}/nginx/nginx.conf",
+      "--pid-path=#{var}/run/nginx.pid",
+      "--lock-path=#{var}/run/nginx.lock",
+      "--http-client-body-temp-path=#{var}/run/nginx/client_body_temp",
+      "--http-proxy-temp-path=#{var}/run/nginx/proxy_temp",
+      "--http-fastcgi-temp-path=#{var}/run/nginx/fastcgi_temp",
+      "--http-uwsgi-temp-path=#{var}/run/nginx/uwsgi_temp",
+      "--http-scgi-temp-path=#{var}/run/nginx/scgi_temp"
+    ]
+
+    puts "adding extra modules"
+
+    args << passenger_config_args if build.include? 'with-passenger'
+    args << "--with-http_dav_module" if build.include? 'with-webdav'
+
+    nginx_modules.each { |name| args << "--with-#{name}"; puts "- adding #{name} module" }
+    add_from_github.each { |name| args <<  "--add-module=#{fetch_from_github(name)}" }
+    add_from_mdounin.each { |name| args <<  "--add-module=#{fetch_from_mdounin(name)}" }
+
+    system "./configure", *args
+    system "make"
+    system "make install"
+    man8.install "objs/nginx.8"
+    (var/'run/nginx').mkpath
+  end
+
+  def fetch_from_github name
+    name, repository = name.split('/')
+    raise "You must specify a repository name for github modules" if repository.nil?
+
+    puts "- adding #{repository} from github..."
+    `git clone -q git://github.com/#{name}/#{repository} modules/#{name}/#{repository}`
+    path = Dir.pwd + '/modules/' + name + '/' + repository
+  end
+
+  def fetch_from_mdounin name
+    name, hash = name.split('#')
+    raise "You must specify a commit sha for mdounin modules" if hash.nil?
+
+    puts "- adding #{name} from mdounin..."
+    `mkdir -p modules/mdounin && cd $_ ; curl -s -O http://mdounin.ru/hg/#{name}/archive/#{hash}.tar.gz; tar -zxf #{hash}.tar.gz`
+    path = Dir.pwd + '/modules/mdounin/' + name + '-' + hash
+  end
+
+  def caveats; <<-EOS.undent
+    In the interest of allowing you to run `nginx` without `sudo`, the default
+    port is set to localhost:8080.
+
+    If you want to host pages on your local machine to the public, you should
+    change that to localhost:80, and run `sudo nginx`. You'll need to turn off
+    any other web servers running port 80, of course.
+
+    You can start nginx automatically on login running as your user with:
+      mkdir -p ~/Library/LaunchAgents
+      cp #{plist_path} ~/Library/LaunchAgents/
+      launchctl load -w ~/Library/LaunchAgents/#{plist_path.basename}
+
+    Though note that if running as your user, the launch agent will fail if you
+    try to use a port below 1024 (such as http's default of 80.)
+    EOS
+  end
+
+  def startup_plist
+    return <<-EOPLIST
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>Label</key>
+    <string>#{plist_name}</string>
+    <key>RunAtLoad</key>
+    <true/>
+    <key>KeepAlive</key>
+    <false/>
+    <key>UserName</key>
+    <string>#{`whoami`.chomp}</string>
+    <key>ProgramArguments</key>
+    <array>
+        <string>#{HOMEBREW_PREFIX}/sbin/nginx</string>
+    </array>
+    <key>WorkingDirectory</key>
+    <string>#{HOMEBREW_PREFIX}</string>
+  </dict>
+</plist>
+    EOPLIST
+  end
+end
+
+__END__
+--- a/conf/nginx.conf
++++ b/conf/nginx.conf
+@@ -33,7 +33,7 @@
+     #gzip  on;
+
+     server {
+-        listen       80;
++        listen       8080;
+         server_name  localhost;
+
+         #charset koi8-r;
--- README.md
+++ README.md
@@ -1,4 +1,98 @@
-formulae
-========
+# formulae
 
 Custom SeatGeek Formula for Homebrew
+
+## Requirements
+
+- Homebrew
+- OS X 10.7. Untested on versions below, YMMV
+- Patience
+
+## Installation
+
+
+You'll want to tap this repository:
+
+    brew tap seatgeek/formulae
+
+And now you can install any of our formula as necessary.
+
+
+## Available formulae
+
+The following formulae will be available when you tap this repository.
+
+### Nginx-Sg
+
+
+> Allows for customized nginx installations
+
+Lines 27-33, 66-73, and 82-98 are the patches to the real existing nginx formula in homebrew.
+
+Usage:
+
+* `--add-github-module=USERNAME/REPOSITORY`: Adds an nginx module from github. Requires `git`.
+* `--add-mdounin-module=MODULE_NAME`: Adds an nginx module from mdounin's hg repo (http://mdounin.ru/hg/)
+* `--include-module-MODULE_NAME`: Adds the `--with-MODULE_NAME` flag for a module that comes with the nginx source but not enabled by default
+
+Example:
+
+		brew install nginx-sg.rb \
+	    --add-github-module=agentzh/chunkin-nginx-module \
+	    --include-module-http_gzip_static_module \
+	    --add-mdounin-module=ngx_http_auth_request_module#a29d74804ff1
+
+Running the above on the commandline produces the following output:
+
+		$ brew install ./nginx-sg.rb --add-github-module=agentzh/chunkin-nginx-module --include-module-http_gzip_static_module --add-mdounin-module=ngx_http_auth_request_module#a29d74804ff1
+		==> Downloading http://nginx.org/download/nginx-1.2.4.tar.gz
+		Already downloaded: /Library/Caches/Homebrew/nginx-sg-1.2.4.tar.gz
+		==> Patching
+		patching file conf/nginx.conf
+		adding extra modules
+		- adding http_gzip_static_module module
+		- adding agentzh from github...
+		- adding ngx_http_auth_request_module from mdounin...
+		==> ./configure --prefix=/usr/local/Cellar/nginx-sg/1.2.4 --with-http_ssl_module --with-pcre --with-ipv6 --with-cc-opt=-I/usr/local/include --with-ld-opt=-L/usr/local/lib --conf-path=/usr/local/etc/nginx/nginx.conf --pid-path=/usr/local/var/run/nginx.pid --lock-path=/
+		==> make
+		==> make install
+		==> Caveats
+		In the interest of allowing you to run `nginx` without `sudo`, the default
+		port is set to localhost:8080.
+
+		If you want to host pages on your local machine to the public, you should
+		change that to localhost:80, and run `sudo nginx`. You'll need to turn off
+		any other web servers running port 80, of course.
+
+		You can start nginx automatically on login running as your user with:
+		  mkdir -p ~/Library/LaunchAgents
+		  cp /usr/local/Cellar/nginx-sg/1.2.4/homebrew.mxcl.nginx-sg.plist ~/Library/LaunchAgents/
+		  launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.nginx-sg.plist
+
+		Though note that if running as your user, the launch agent will fail if you
+		try to use a port below 1024 (such as http's default of 80.)
+		==> Summary
+		/usr/local/Cellar/nginx-sg/1.2.4: 9 files, 900K, built in 28 seconds
+
+
+## License
+
+Copyright (c) 2012 SeatGeek and other contributors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
